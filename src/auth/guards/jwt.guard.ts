09d94427796418e15ee3b2a913fa79f88a5f5fc9
mahmoud hamdy <72884628+m-hamdy1572@users.noreply.github.com>
2024-01-20 19:29:20 +0200

    add

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,16 +1,18 @@
-import { ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
+@Injectable()
 export class JwtAuthGuard extends AuthGuard() {
   canActivate(context: ExecutionContext) {
-    const bearerToken = context.switchToHttp().getRequest().headers.authorization;
+    const bearerToken = context.switchToHttp().getRequest()
+      .headers.authorization;
     if (!bearerToken || !bearerToken.startsWith('Bearer ')) {
       throw new UnauthorizedException('No Bearer token provided');
     }
     return super.canActivate(context);
   }
 
-  // This method is called by passport if the token is valid 
+  // This method is called by passport if the token is valid
   handleRequest(err, user, info) {
     if (info && info['name'] === 'TokenExpiredError') {
       throw new UnauthorizedException(`Token expired, please login again...`);
